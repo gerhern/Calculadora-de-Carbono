da9411e0c9a27445099bf1d27522e48b089b989d
Carmine <carmine112255@gmail.com>
2021-10-20 00:36:25 -0500

    Tooltip de imagenes(usando tooltip de index) y secureassets para web

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
 
 </head>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -147,5 +147,5 @@
     </div>
 
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
-    <script src="{{ asset('js/slide.js') }}"></script>
+    <script src="{{ secure_asset('js/slide.js') }}"></script>
 @endsection
