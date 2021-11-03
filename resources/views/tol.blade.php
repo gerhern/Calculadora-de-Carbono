a0c4d2042a777a562c8123446ac95e8df8bcc75c
Carmine <carmine112255@gmail.com>
2021-11-02 23:53:21 -0600

    Simplificacion mejora de calcular compensacion

--- resources/views/tol.blade.php
+++ resources/views/tol.blade.php
@@ -7,7 +7,7 @@
         {{-- div de formulario de calculo --}}
         <div class="">
             {{-- Titulo --}}
-            <div class="">
+            <div>
                 <h2 class="  py-16 px-10 text-center text-2xl mb-6 lg:text-5xl italic">Cálculo de árboles para
                     compensación de carbono</h2>
             </div>
@@ -53,10 +53,10 @@
                         class="text-forest font-bold italic">{{ $footprint }} </span> toneladas de CO<sub>2</sub>
                     debes plantar alguno de los siguientes tipos de árboles*.</p>
 
-                {{-- Tablas --}}
+                {{-- Resultados --}}
                 <div class="lg:my-8">
 
-                    {{-- Tabla1 --}}
+                    {{-- Arboles --}}
                     <div class="my-2 flex flex-wrap justify-evenly w-full ">
                         @foreach ($data['data'] as $tree)
                             <div class="m-2 rounded-md shadow-2xl flex lg:flex-col has-tooltip relative">
@@ -68,7 +68,8 @@
                                     <h3 class="lg:text-xl text-md px-2 w-full text-center font-bold">{{ $tree[1] }}</h3>
                                     {{-- cantidad --}}
                                     <h3 class="lg:text-4xl text-3xl my-2 px-2 w-full text-center">{{ $tree[5] }}
-                                    <p class="lg:text-3xl text-lg">individuos</p></h3>
+                                        <p class="lg:text-3xl text-lg">individuos</p>
+                                    </h3>
                                     <h3 class="lg:text-lg text-sm px-2 w-full font-semibold">Nombre cientifico:</h3>
                                     <h3 class="lg:text-lg text-sm px-2 w-full italic">{{ $tree[2] }}</h3>
                                     <p class="lg:text-lg text-sm px-2 w-full font-semibold">Capacidad de absorcion:</p>
@@ -81,30 +82,21 @@
 
                 </div>
 
+                {{-- Indicaciones --}}
                 <div class="py-6 w-11/12 mx-auto border border-black bg-black shadow-md bg-opacity-10 my-8 rounded-md">
                     <p class="leading-tight lg:text-xl text-lg p-3">Para llegar a la cantidad estimada de árboles de
                         compensación de huella de carbono se uso la siguiente metodología.</p>
-                    <ol>
-                        <li>
-                            <p class="leading-tight lg:text-xl text-lg p-3">1.-Se realiza la conversión de toneladas a
-                                kilogramos de CO<sub>2</sub> ingresadas por el usuario (ej. 128 ton = (128 * 1,000) por lo tanto
-                                128 ton = 128,000 kg).</p>
-                        </li>
-                        <li>
-                            <p class="leading-tight lg:text-xl text-lg p-3">2.-Se toma como referencia la capacidad de absorción
-                                por especie en un periodo de un año (ej. Roble -> capacidad de absorción = 229.28 kg/año).</p>
-                        </li>
-                        <li>
-                            <p class="leading-tight lg:text-xl text-lg p-3">3.-Se divide la huella de carbono total en
-                                kilogramos entre la capacidad de absorción de la especie seleccionada (ej. Cantidad de robles =
-                                (128,000 / 229.28) por lo tanto 128,000 kg = 558.2693 robles ).</p>
-                        </li>
-                        <li>
-                            <p class="leading-tight lg:text-xl text-lg p-3">4.-Dado que no se pueden plantar fracciones de
-                                árboles se redondea el resultado (ej. 558.2693 -> 558 robles).</p>
-                        </li>
 
-                    </ol>
+                    <p class="leading-tight lg:text-xl text-lg p-3">1.-Se realiza la conversión de toneladas a
+                        kilogramos de CO<sub>2</sub> ingresadas por el usuario (ej. 128 ton = (128 * 1,000) por lo tanto
+                        128 ton = 128,000 kg).</p>
+                    <p class="leading-tight lg:text-xl text-lg p-3">2.-Se toma como referencia la capacidad de absorción
+                        por especie en un periodo de un año (ej. Roble -> capacidad de absorción = 229.28 kg/año).</p>
+                    <p class="leading-tight lg:text-xl text-lg p-3">3.-Se divide la huella de carbono total en
+                        kilogramos entre la capacidad de absorción de la especie seleccionada (ej. Cantidad de robles =
+                        (128,000 / 229.28) por lo tanto 128,000 kg = 558.2693 robles ).</p>
+                    <p class="leading-tight lg:text-xl text-lg p-3">4.-Dado que no se pueden plantar fracciones de
+                        árboles se redondea el resultado (ej. 558.2693 -> 558 robles).</p>
 
                     <p class="leading-tight lg:text-xl text-lg p-3">-Para plantar árboles debes de asegurarte que los
                         especímenes no tengan plagas o enfermedades y estos tengan una altura óptima de entre 1 metro a 1.5 y no
@@ -113,13 +105,14 @@
                     <p class="leading-tight lg:text-2xl text-lg p-3 italic font-semibold">*La capacidad de absorción de carbono
                         puede variar dependiendo del crecimiento del individuo, su diámetro a la altura del pecho (DAP) y a la
                         zona donde se localiza.</p>
-                    <p class="leading-tight text-xs px-3">Fuente: Guía para la plantación de árboles del municipio de Mérida(2018)</p>
-                    <a class="leading-tight text-xs px-3 underline"
+                    <p class="leading-tight text-xs px-3">Fuente: Guía para la plantación de árboles del municipio de
+                        Mérida(2018)</p>
+                    <a class="leading-tight text-sm px-3 underline"
                         href="http://www.merida.gob.mx/sustentable/contenidos/doc/guia-de-plantacion-de-arboles.pdf">Guia-de-plantacion-de-arboles.pdf</a>
                 </div>
                 {{-- boton de compensar --}}
-                
-                    <x-button route="compensar" text="Compensar"></x-button>
+
+                <x-button route="compensar" text="Compensar"></x-button>
 
             @endisset
         </div>
